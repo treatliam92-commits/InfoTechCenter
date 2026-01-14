cf9b8656fe24f0b7fa62ccd2a09647a39b65606d
Darkining__ <treatliam92@gmail.com>
2026-01-14 12:21:53 -0500

    Made ... dissapear 95% stable

--- WithOrWithoutMarinara.py
+++ WithOrWithoutMarinara.py
@@ -4,14 +4,13 @@ import sys
 import time
 print ("Welcome Branch - Developer: Liam Treat")
 print("\nWelcome to YOUR DOOM / InfoTechCenter v.1.0")
-print("\nInfoTechCenter is Booting Up...")
 x = 0
 ellipsis = 0
 while x != 20:
     x += 1
     ellipsisMessage = ("InfoTechCenter OS Booting Up" + "." * ellipsis)
     ellipsis += 1
-    sys.stdout.write("\r" + ellipsisMessage)
+    sys.stdout.write("\r" + ellipsisMessage + "   ")
     time.sleep(.5)
     if ellipsis == 4:
         ellipsis = 0
