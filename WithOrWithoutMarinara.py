876dd8d5e15ec5e6808204fd6ea361480bb6077b
Darkining__ <treatliam92@gmail.com>
2026-01-26 13:36:38 -0500

    Started code

--- WithOrWithoutMarinara.py
+++ WithOrWithoutMarinara.py
@@ -1,1 +1,12 @@
 #Gasoline Branch
+# def = Short for define. It tells Python you’re about to define a function.
+#
+def gas_tank_level (current_amount, tank_capacity):
+    percent_full = (current_amount / tank_capacity) * 100
+    return round(percent_full, 1)
+
+current_gas = float(input("Enter current gas amount: "))
+tank_capacity = float(input("Enter total tank capacity: "))
+
+level = gas_tank_level(current_gas, tank_capacity)
+print(f"Gas tank level: {level}%")
