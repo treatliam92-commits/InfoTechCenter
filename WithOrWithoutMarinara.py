d20961aaec80d8c1ca90c32d8e3bb5a806867f5e
Darkining__ <treatliam92@gmail.com>
2026-01-26 13:51:51 -0500

    Finnished Gasoline code 100% stable

--- WithOrWithoutMarinara.py
+++ WithOrWithoutMarinara.py
@@ -1,12 +1,18 @@
 #Gasoline Branch
+# 
+import random
 # def = Short for define. It tells Python you’re about to define a function.
-#
-def gas_tank_level (current_amount, tank_capacity):
+def random_gas_tank_status(tank_capacity=50):
+    current_amount = random.uniform(0, tank_capacity)
     percent_full = (current_amount / tank_capacity) * 100
-    return round(percent_full, 1)
+    percent_full = round(percent_full, 1)
 
-current_gas = float(input("Enter current gas amount: "))
-tank_capacity = float(input("Enter total tank capacity: "))
+    print(f"⛽ Gas tank level: {percent_full}%")
 
-level = gas_tank_level(current_gas, tank_capacity)
-print(f"Gas tank level: {level}%")
+    if percent_full < 25:
+        print("⚠️ Low fuel — please go to the gas station.")
+    if percent_full > 25:
+        print ("Looking good. Drive safe :)")
+
+# Example usage
+random_gas_tank_status()
